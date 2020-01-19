8cef7c460202f42ca8e73c96b6c7b3c9447253d7
LucasPLopes <l.lopes.fga@gmail.com>
2020-01-19 17:47:40 -0300

    Rotas get por Id, post, delete OK Put e get para todos nao funcionando

--- apis/helpdevs.net/server/routes/user.js
+++ apis/helpdevs.net/server/routes/user.js
@@ -4,10 +4,10 @@ const user  = require('../controller/user')
 const router = express.Router()
 
 
-router.route('/').get(user.getAll)
-router.route('/:id').get(user.get)
-router.route('/').post(user.post)
+router.route('/').get(user.getAll) 
+router.route('/:id').get(user.get) // ok
+router.route('/').post(user.post) //ok 
 router.route('/').put(user.put)
-router.route('/:id').delete(user.del)
+router.route('/:id').delete(user.del) // ok
 
 module.exports = router;
